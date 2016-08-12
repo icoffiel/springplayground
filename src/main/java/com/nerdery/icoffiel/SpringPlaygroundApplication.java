9399dcf4e142ec3839fb6f304d1bd92af2ec16c9
icoffiel <iain.coffield@nerdery.com>
2016-08-12 11:04:29 -0500

    feat(maven): Implementing maven profiles
    Implementing the dev and prod profiles for maven

--- src/main/java/com/nerdery/icoffiel/SpringPlaygroundApplication.java
+++ src/main/java/com/nerdery/icoffiel/SpringPlaygroundApplication.java
@@ -2,11 +2,18 @@ package com.nerdery.icoffiel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringPlaygroundApplication {
+public class SpringPlaygroundApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringPlaygroundApplication.class);
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(SpringPlaygroundApplication.class, args);
 	}
 }
